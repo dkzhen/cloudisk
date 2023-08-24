49015a56a2753feb9b99a369b45ed6d255e7f504
dkzhen <dani314123@gmail.com>
2023-08-24 07:43:57 +0700

    Hero v1.0

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Hero() {
   return (
     <div className="flex flex-col md:mt-8 ">
-      <div className="md:text-5xl text-3xl font-Lora font-bold ">Cloudisk.</div>
+      <div className="md:text-5xl text-3xl font-Lora font-bold hidden md:block ">
+        Cloudisk.
+      </div>
       <div className="font-AmerType font-medium text-xl md:text-2xl">
         Hi , Welcome To Cloudisk.
       </div>
