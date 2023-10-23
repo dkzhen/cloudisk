052007521a86aa12e66ab7ef74f1b60bb4d636bc
dkzhen <dani314123@gmail.com>
2023-10-23 14:01:22 +0700

    feat: mod : remove env config

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -1,7 +1,10 @@
+"use client";
+
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore/lite";
-
+import dotenv from "dotenv";
+dotenv.config();
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -11,6 +14,8 @@ const firebaseConfig = {
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
 };
+
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_KEY: "AIzaSyD5GvJvNuzGSsA0BixK-8F4a86Lc89wIsA",
-    AUTH_DOMAIN: "zhen-702f1.firebaseapp.com",
-    PROJECT_ID: "zhen-702f1",
-    STORAGE_BUCKET: "zhen-702f1.appspot.com",
-    MESSAGING_SENDER_ID: "277503513519",
-    APP_ID: "1:277503513519:web:00388e161340280553155a",
-    MEASUREMENT_ID: "G-5ZEXJT1KC6",
-  },
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
