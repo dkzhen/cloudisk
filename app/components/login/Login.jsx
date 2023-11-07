257a882d284c6776cdc5ea101b4be929e2c92acd
dkzhen <dani314123@gmail.com>
2023-11-07 22:28:07 +0700

    mod: dark mode login

--- app/components/login/Login.jsx
+++ app/components/login/Login.jsx
@@ -66,12 +66,12 @@ export default function Login() {
                       width={0}
                       height={0}
                     />
-                    <span className="block w-max font-semibold tracking-wide text-white text-sm transition duration-300 group-hover:text-blue-600 group-focus:text-blue-600 group-active:text-blue-600  sm:text-base">
+                    <span className="block w-max font-semibold tracking-wide text-black dark:text-white text-sm transition duration-300 group-hover:text-blue-600 group-focus:text-blue-600 group-active:text-blue-600  sm:text-base">
                       {loadingGoogle ? "Loading..." : "Continue with Google"}
                     </span>
                   </div>
                 </button>
-                <h2 className="mb-8 text-xl text-white  font-bold">
+                <h2 className="mb-8 text-xl text-black dark:text-white  font-bold">
                   Log in account
                 </h2>
 
@@ -89,10 +89,10 @@ export default function Login() {
                     <input
                       type="email"
                       onChange={(e) => setEmail(e.target.value)}
-                      className="peer h-full w-full rounded-[7px] border border-blue-gray-200  border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-white   outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white focus:border-2 focus:border-blue-600 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                      className="peer h-full w-full rounded-[7px] border border-blue-gray-200  border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-black dark:text-white   outline outline-0 transition-all placeholder-shown:border-gray-400 dark:placeholder-shown:border-white placeholder-shown:border-t-gray-400 dark:placeholder-shown:border-t-white  focus:border-2 focus:border-blue-600 dark:focus:border-blue-600 dark:focus:border-t-transparent focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       placeholder=" "
                     />
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-600 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-600 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white">
+                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-black dark:peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-600 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-600 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white">
                       Email
                     </label>
                   </div>
@@ -101,11 +101,11 @@ export default function Login() {
                   <div className="relative h-10 w-full min-w-[200px]">
                     <input
                       type="password"
-                      className="peer h-full w-full rounded-[7px] border border-blue-gray-200  border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-white   outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-white placeholder-shown:border-t-white focus:border-2 focus:border-blue-600 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                      className="peer h-full w-full rounded-[7px] border border-blue-gray-200  border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-black dark:text-white   outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-gray-400 dark:placeholder-shown:border-white placeholder-shown:border-t-gray-400 dark:placeholder-shown:border-t-white  focus:border-2 focus:border-blue-600 dark:focus:border-blue-600 dark:focus:border-t-transparent focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                       placeholder=" "
                       onChange={(e) => setPass(e.target.value)}
                     />
-                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-600 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-600 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white">
+                    <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-black dark:peer-placeholder-shown:text-white peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-blue-600 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-blue-600 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-blue-600 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-white dark:border-white">
                       Password
                     </label>
                   </div>
@@ -116,7 +116,7 @@ export default function Login() {
                   className="group  h-10 px-3 border-2 border-gray-300 rounded-lg transition duration-300 hover:border-blue-400 flex items-center justify-center focus:bg-blue-50 active:bg-blue-100"
                 >
                   <div className="relative flex items-center space-x-4 justify-center">
-                    <span className="block w-max font-semibold tracking-wide text-white text-sm transition duration-300 group-hover:text-blue-600 sm:text-base group-focus:text-blue-600 group-active:text-blue-600">
+                    <span className="block w-max font-semibold tracking-wide text-gray-600 dark:text-white text-sm transition duration-300 group-hover:text-blue-600 sm:text-base group-focus:text-blue-600 group-active:text-blue-600">
                       {loadingEmail ? "Loading.." : "Log in"}
                     </span>
                   </div>
